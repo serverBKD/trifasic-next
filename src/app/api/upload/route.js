02cb469c8dc726ea98ac43f733c539e476a1d481
serverBKD <serverbkd@outlook.com>
2023-12-13 21:34:49 -0400

    20231213: v0.0.2: insert / api/upload + subida de archivos carpeta public:OK

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,27 @@
-import { NextResponse } from 'next/server'
+import { NextRequest,NextResponse } from 'next/server'
+import { writeFile } from 'fs/promises'
+import path from 'path'
 
 export function GET() {
 	return NextResponse.json({'message':'uploading files...'})
 }
 
-export function POST() {
-	return NextResponse.json({'message':'uploaded files...'})
+export async function POST(request) {
+	const data = await request.formData()
+	const image = data.get('file')
+
+	if (!image) {
+		return NextResponse.json({'message':'no image'})
+	}
+	
+	//. Convertir FormData en Buffer: Archivo cargado en Memoria RAM
+	const bytes = await image.arrayBuffer()
+	const buffer = Buffer.from(bytes)
+
+	//. Guardar archivo en Carpeta Public
+	const imagePath = `${new Date().getTime()}-x-${image.name}`
+	const filePath = path.join(process.cwd(),'public','uploads',imagePath)
+	await writeFile(filePath,buffer)
+
+	return NextResponse.json({'message': image})
 }
