24a9248bb5d218a6efdea7beeb52659b118fff46
serverBKD <serverbkd@outlook.com>
2023-12-16 15:07:09 -0400

    20231216: v0.1.1: useForm LandingPage

--- src/components/CTAForm.jsx
+++ src/components/CTAForm.jsx
@@ -1,4 +1,18 @@
+'use client'
+import { useForm } from 'react-hook-form'
+
 export default function CTAForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm({})
+
+	const onSubmit = handleSubmit((data) => {
+		console.log(data)
+	})
+
 	return (
 		<section className='text-gray-400 bg-slate-600 body-font'>
 			<div className='container px-5 py-24 mx-auto flex flex-wrap items-center'>
@@ -13,23 +27,36 @@ export default function CTAForm() {
 						experiencia en solucionar.
 					</p>
 				</div>
-				<div className='lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0'>
+				<form
+					onSubmit={onSubmit}
+					className='lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0'
+				>
 					<h2 className='text-white text-lg font-medium title-font mb-5'>
 						Contacto
 					</h2>
 					<div className='relative mb-4'>
 						<label
-							htmlFor='full-name'
+							htmlFor='fullname'
 							className='leading-7 text-sm text-gray-400'
 						>
 							Nombre Completo
 						</label>
 						<input
 							type='text'
-							id='full-name'
-							name='full-name'
+							id='fullname'
+							name='fullname'
 							className='w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-[#0086C3] rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+							{...register('fullname', {
+								required: {
+									value: true,
+									message: 'Introduzca su Nombre y Apellido',
+								},
+								min: 4,
+							})}
 						/>
+						{errors.fullname && (
+							<span>{errors.fullname?.message}</span>
+						)}
 					</div>
 					<div className='relative mb-4'>
 						<label
@@ -43,7 +70,15 @@ export default function CTAForm() {
 							id='email'
 							name='email'
 							className='w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-[#0086C3] rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+							{...register('email', {
+								required: {
+									value: true,
+									message: 'email no válido',
+								},
+								pattern: 'xxxx@gmail.com',
+							})}
 						/>
+						{errors.email && <span>{errors.email?.message}</span>}
 					</div>
 					<div className='relative mb-4'>
 						<label
@@ -57,7 +92,15 @@ export default function CTAForm() {
 							id='tel'
 							name='tel'
 							className='w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-[#0086C3] rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+							{...register('tel', {
+								required: {
+									value: true,
+									message: 'introduce un número de contacto',
+								},
+								pattern: '04xx-xxxxxxx',
+							})}
 						/>
+						{errors.tel && <span>{errors.tel?.message}</span>}
 					</div>
 					<button className='bg-[#0086C3] text-[#EFCA0A] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
 						Enviar Info
@@ -65,7 +108,7 @@ export default function CTAForm() {
 					<p className='text-xs mt-3'>
 						Pronto un asesor se pondrá en contacto.
 					</p>
-				</div>
+				</form>
 			</div>
 		</section>
 	)
